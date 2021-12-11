cb00900bb9697e5fbea527d6d30aff33a66936ad
SajadRahimi1 <sajadrahimi6633@gmail.com>
2021-12-11 08:59:01 +0330

    Change example/lib/main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,11 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Persian Fonts Example',
-        home: MyHomePage(),
-        theme: ThemeData(
-          textTheme: PersianFonts.vazirTextTheme,
-        ));
+      title: 'Persian Fonts Example',
+      home: MyHomePage(),
+      theme: ThemeData(
+        textTheme: PersianFonts.vazirTextTheme,
+      )
+    );
   }
 }
 
